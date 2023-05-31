58741c277046948d7439e722d9bc3d041cf91816
shindevinud <121563011+shindevinud@users.noreply.github.com>
2023-05-31 13:53:33 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -12,7 +12,7 @@ def newDeploy(workspace,ip,context)
   sh "scp /var/lib/jenkins/workspace/${workspace}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
 }
 
-def newTesting
+def newTesting(repo)
 {
     git "${repo}"
 }
